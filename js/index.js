7d6a81f1c3584fe6b2336eb24a60a2aedfe3d980
Đào Duy Nam <lenkc98@gmail.com>
2018-06-24 23:48:07 +0700

    Add login page

--- js/index.js
+++ js/index.js
@@ -8,6 +8,9 @@ function statusChangeCallback(r) {
     if (r.status != 'connected') {
         window.location = '../login.html';
     }
+    else {
+        window.location = '../';
+    }
 }
 window.onload = function() {
     checkLoginState();
